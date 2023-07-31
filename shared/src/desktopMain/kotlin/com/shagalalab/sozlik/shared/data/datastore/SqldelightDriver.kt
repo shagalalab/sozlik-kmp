69dd3261b5927bcd0191de277b0e0070dbd9ef5a
Atabek <atabekmur@gmail.com>
2023-07-31 23:06:54 +1000

    Change desktop db from in memory to file

--- shared/src/desktopMain/kotlin/com/shagalalab/sozlik/shared/data/datastore/SqldelightDriver.kt
+++ shared/src/desktopMain/kotlin/com/shagalalab/sozlik/shared/data/datastore/SqldelightDriver.kt
@@ -7,7 +7,7 @@ import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 internal class JvmSqldelightDriver : SqldelightDriver {
     override val driver: SqlDriver
         get() {
-            val driver: SqlDriver = JdbcSqliteDriver(JdbcSqliteDriver.IN_MEMORY)
+            val driver: SqlDriver = JdbcSqliteDriver("jdbc:sqlite:sozlik.db")
             SozlikDatabase.Schema.create(driver)
             return driver
         }
